a1c64ca63ed6c8518dda59702d01b20897f94cb8
Sujalofficial07 <sujalbaloda32@gmail.com>
2026-01-04 14:15:15 +0530

    Create StatType.java

---
+++ src/main/java/com/sujal/skyblocksandbox/stats/StatType.java
@@ -0,0 +1,24 @@
+package com.sujal.skyblocksandbox.stats;
+
+public enum StatType {
+    HEALTH("Health", "❤", 100.0),
+    DEFENSE("Defense", "❈", 0.0),
+    MANA("Intelligence", "✎", 100.0),
+    STRENGTH("Strength", "❁", 0.0),
+    CRIT_CHANCE("Crit Chance", "☣", 30.0), // Base 30%
+    SPEED("Speed", "✦", 100.0);
+
+    private final String name;
+    private final String icon;
+    private final double baseValue;
+
+    StatType(String name, String icon, double baseValue) {
+        this.name = name;
+        this.icon = icon;
+        this.baseValue = baseValue;
+    }
+
+    public String getName() { return name; }
+    public String getIcon() { return icon; }
+    public double getBaseValue() { return baseValue; }
+}
