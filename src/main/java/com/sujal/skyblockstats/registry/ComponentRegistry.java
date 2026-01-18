5f50709fb5beb0436385c94bf92cc60f325b99ef
Sujalofficial07 <sujalbaloda32@gmail.com>
2026-01-18 16:18:21 +0530

    Update ComponentRegistry.java

--- src/main/java/com/sujal/skyblockstats/registry/ComponentRegistry.java
+++ src/main/java/com/sujal/skyblockstats/registry/ComponentRegistry.java
@@ -3,33 +3,24 @@ package com.sujal.skyblockstats.registry;
 import com.sujal.skyblockstats.components.PlayerSkyBlockComponent;
 import dev.onyxstudios.cca.api.v3.component.ComponentKey;
 import dev.onyxstudios.cca.api.v3.component.ComponentRegistryV3;
-import dev.onyxstudios.cca.api.v3.component.ComponentRegistrationInitializer;
-import dev.onyxstudios.cca.api.v3.component.ComponentRegistrationContext;
 import dev.onyxstudios.cca.api.v3.entity.EntityComponentFactoryRegistry;
 import dev.onyxstudios.cca.api.v3.entity.EntityComponentInitializer;
 import dev.onyxstudios.cca.api.v3.entity.RespawnCopyStrategy;
 import net.minecraft.util.Identifier;
 
-public class ComponentRegistry implements EntityComponentInitializer, ComponentRegistrationInitializer {
+public class ComponentRegistry implements EntityComponentInitializer {
 
-    // Component Key Definition
+    // Component Key definition
     public static final ComponentKey<PlayerSkyBlockComponent> SKYBLOCK_STATS = 
         ComponentRegistryV3.INSTANCE.getOrCreate(new Identifier("skyblockstats", "stats"), PlayerSkyBlockComponent.class);
 
-    // 1. Register the Component itself (Required for CCA 5+)
-    @Override
-    public void registerComponents(ComponentRegistrationContext context) {
-        // Explicitly registering is strictly required in newer CCA versions if getOrCreate is called statically
-        // But simply having this method and the entrypoint allows CCA to discover the static Key.
-    }
-
-    // 2. Attach the Component to Players
     @Override
     public void registerEntityComponentFactories(EntityComponentFactoryRegistry registry) {
+        // Attach component to Players
         registry.registerForPlayers(SKYBLOCK_STATS, PlayerSkyBlockComponent::new, RespawnCopyStrategy.ALWAYS_COPY);
     }
     
     public static void init() {
-        // Static init to ensure class loading
+        // Static init if needed
     }
 }
