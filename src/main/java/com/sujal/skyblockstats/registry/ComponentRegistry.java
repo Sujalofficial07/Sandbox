bfc73d31f3a08cbfc1a52d1665d7906864f03d73
Sujalofficial07 <sujalbaloda32@gmail.com>
2026-01-18 16:05:39 +0530

    Implement component registration in ComponentRegistry
    Added ComponentRegistrationInitializer to ComponentRegistry and implemented registerComponents method.

--- src/main/java/com/sujal/skyblockstats/registry/ComponentRegistry.java
+++ src/main/java/com/sujal/skyblockstats/registry/ComponentRegistry.java
@@ -3,22 +3,33 @@ package com.sujal.skyblockstats.registry;
 import com.sujal.skyblockstats.components.PlayerSkyBlockComponent;
 import dev.onyxstudios.cca.api.v3.component.ComponentKey;
 import dev.onyxstudios.cca.api.v3.component.ComponentRegistryV3;
+import dev.onyxstudios.cca.api.v3.component.ComponentRegistrationInitializer;
+import dev.onyxstudios.cca.api.v3.component.ComponentRegistrationContext;
 import dev.onyxstudios.cca.api.v3.entity.EntityComponentFactoryRegistry;
 import dev.onyxstudios.cca.api.v3.entity.EntityComponentInitializer;
 import dev.onyxstudios.cca.api.v3.entity.RespawnCopyStrategy;
 import net.minecraft.util.Identifier;
 
-public class ComponentRegistry implements EntityComponentInitializer {
+public class ComponentRegistry implements EntityComponentInitializer, ComponentRegistrationInitializer {
 
+    // Component Key Definition
     public static final ComponentKey<PlayerSkyBlockComponent> SKYBLOCK_STATS = 
         ComponentRegistryV3.INSTANCE.getOrCreate(new Identifier("skyblockstats", "stats"), PlayerSkyBlockComponent.class);
 
+    // 1. Register the Component itself (Required for CCA 5+)
+    @Override
+    public void registerComponents(ComponentRegistrationContext context) {
+        // Explicitly registering is strictly required in newer CCA versions if getOrCreate is called statically
+        // But simply having this method and the entrypoint allows CCA to discover the static Key.
+    }
+
+    // 2. Attach the Component to Players
     @Override
     public void registerEntityComponentFactories(EntityComponentFactoryRegistry registry) {
         registry.registerForPlayers(SKYBLOCK_STATS, PlayerSkyBlockComponent::new, RespawnCopyStrategy.ALWAYS_COPY);
     }
     
     public static void init() {
-        // Initialization logic if needed
+        // Static init to ensure class loading
     }
 }
