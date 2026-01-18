480fc2478e43247706d49340a62ea8460af693e4
Sujalofficial07 <sujalbaloda32@gmail.com>
2026-01-18 10:59:19 +0530

    Create ComponentRegistry.java

---
+++ src/main/java/com/sujal/skyblockstats/registry/ComponentRegistry.java
@@ -0,0 +1,25 @@
+package com.sujal.skyblockstats.registry;
+
+import com.sujal.skyblockstats.components.PlayerSkyBlockComponent;
+import dev.onyxstudios.cca.api.v3.component.ComponentKey;
+import dev.onyxstudios.cca.api.v3.component.ComponentRegistryV3;
+import dev.onyxstudios.cca.api.v3.entity.EntityComponentFactoryRegistry;
+import dev.onyxstudios.cca.api.v3.entity.EntityComponentInitializer;
+import dev.onyxstudios.cca.api.v3.entity.RespawnCopyStrategy;
+import net.minecraft.util.Identifier;
+
+public class ComponentRegistry implements EntityComponentInitializer {
+
+    public static final ComponentKey<PlayerSkyBlockComponent> SKYBLOCK_STATS = 
+        ComponentRegistryV3.INSTANCE.getOrCreate(new Identifier("skyblockstats", "stats"), PlayerSkyBlockComponent.class);
+
+    @Override
+    public void registerEntityComponentFactories(EntityComponentFactoryRegistry registry) {
+        registry.registerForPlayers(SKYBLOCK_STATS, PlayerSkyBlockComponent::new, RespawnCopyStrategy.ALWAYS_COPY);
+    }
+    
+    public static void init() {
+        // Static initializer, called from main mod if needed for side effects, 
+        // though CCA handles auto-init via entrypoint.
+    }
+}
