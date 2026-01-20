3eaf639c77abd942cd3d854416e25b3ae4b14695
Sujalofficial07 <sujalbaloda32@gmail.com>
2026-01-20 16:32:37 +0530

    Create SkyblockStatsMod.java

---
+++ skyblock-stats/src/main/java/com/sujal/skyblock/stats/SkyblockStatsMod.java
@@ -0,0 +1,15 @@
+package com.sujal.skyblock.stats;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SkyblockStatsMod implements ModInitializer {
+    public static final String MOD_ID = "skyblock-stats";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Skyblock Stats Engine Loaded");
+    }
+}
