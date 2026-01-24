4c61ae9b1f97f9d9f3ab520814ac0dd11b7c41c6
Sujalofficial07 <sujalbaloda32@gmail.com>
2026-01-24 17:37:15 +0530

    Update SkyblockItemsMod.java

--- skyblock-items/src/main/java/com/sujal/skyblock/items/SkyblockItemsMod.java
+++ skyblock-items/src/main/java/com/sujal/skyblock/items/SkyblockItemsMod.java
@@ -1,12 +1,13 @@
 package com.sujal.skyblock.items;
 
-import com.sujal.skyblock.items.menu.MenuHandler;
+import com.sujal.skyblock.items.menu.SkyblockMenuHandler;
+import com.sujal.skyblock.items.registry.SkyblockRegistry;
 import net.fabricmc.api.ModInitializer;
 
 public class SkyblockItemsMod implements ModInitializer {
     @Override
     public void onInitialize() {
-        Items.register(); // Load all items like Hyperion
-        MenuHandler.register(); // Enable /sbitems command
+        SkyblockRegistry.registerAll(); // Load Items & Groups
+        SkyblockMenuHandler.register(); // Load Command
     }
 }
