eeb997a410d8233da67380d17131664e89d8c757
Sujalofficial07 <sujalbaloda32@gmail.com>
2026-01-24 20:36:58 +0530

    Update SkyblockUIMod.java

--- skyblock-ui/src/main/java/com/sujal/skyblock/ui/SkyblockUIMod.java
+++ skyblock-ui/src/main/java/com/sujal/skyblock/ui/SkyblockUIMod.java
@@ -1,12 +1,31 @@
-public class SkyblockUIMod implements ModInitializer {
+package com.sujal.skyblock.ui;
+
+import com.sujal.skyblock.ui.hud.ScoreboardRenderer;
+import com.sujal.skyblock.ui.menu.ProfileMenu;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.server.command.CommandManager;
+
+public class SkyblockUIMod implements ModInitializer, ClientModInitializer {
+    
+    // Server-side Logic
     @Override
     public void onInitialize() {
-         CommandRegistrationCallback.EVENT.register((dispatcher, registry, env) -> {
+        CommandRegistrationCallback.EVENT.register((dispatcher, registry, env) -> {
             dispatcher.register(CommandManager.literal("sbmenu")
                 .executes(ctx -> {
                     ProfileMenu.open(ctx.getSource().getPlayerOrThrow());
                     return 1;
                 }));
         });
+    }
+
+    // Client-side Logic (Renderer)
+    @Override
+    public void onInitializeClient() {
+        // Register the Scoreboard
+        HudRenderCallback.EVENT.register(new ScoreboardRenderer());
     }
 }
