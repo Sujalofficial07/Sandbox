74bf4bc3c27c984fa88227a8d72210d391b30465
Sujalofficial07 <sujalbaloda32@gmail.com>
2026-01-24 20:57:29 +0530

    Update SkyblockUIMod.java

--- skyblock-ui/src/main/java/com/sujal/skyblock/ui/SkyblockUIMod.java
+++ skyblock-ui/src/main/java/com/sujal/skyblock/ui/SkyblockUIMod.java
@@ -1,6 +1,7 @@
 package com.sujal.skyblock.ui;
 
 import com.sujal.skyblock.ui.hud.ScoreboardRenderer;
+import com.sujal.skyblock.ui.menu.BankMenu;
 import com.sujal.skyblock.ui.menu.ProfileMenu;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.ModInitializer;
@@ -14,16 +15,22 @@ public class SkyblockUIMod implements ModInitializer, ClientModInitializer {
     @Override
     public void onInitialize() {
         CommandRegistrationCallback.EVENT.register((dispatcher, registry, env) -> {
-            // Register /sb menu here because UI module knows about ProfileMenu
             dispatcher.register(CommandManager.literal("sb")
+                // /sb menu OR /sb profile
                 .then(CommandManager.literal("menu")
                     .executes(ctx -> {
                         ProfileMenu.open(ctx.getSource().getPlayerOrThrow());
                         return 1;
                     }))
-                .then(CommandManager.literal("profile") // Alias for menu
+                .then(CommandManager.literal("profile")
                     .executes(ctx -> {
                         ProfileMenu.open(ctx.getSource().getPlayerOrThrow());
+                        return 1;
+                    }))
+                // /sb bank (Moved here from Economy module)
+                .then(CommandManager.literal("bank")
+                    .executes(ctx -> {
+                        BankMenu.open(ctx.getSource().getPlayerOrThrow());
                         return 1;
                     }))
             );
@@ -33,7 +40,6 @@ public class SkyblockUIMod implements ModInitializer, ClientModInitializer {
     // Client-side Logic (Renderer)
     @Override
     public void onInitializeClient() {
-        // Register the Scoreboard
         HudRenderCallback.EVENT.register(new ScoreboardRenderer());
     }
 }
