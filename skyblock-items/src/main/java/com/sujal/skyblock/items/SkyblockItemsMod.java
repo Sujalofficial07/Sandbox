43ef3e4f09f4d4a5d099cbc05dfac599ef47a7a4
Sujalofficial07 <sujalbaloda32@gmail.com>
2026-01-24 14:46:48 +0530

    Update SkyblockItemsMod.java

--- skyblock-items/src/main/java/com/sujal/skyblock/items/SkyblockItemsMod.java
+++ skyblock-items/src/main/java/com/sujal/skyblock/items/SkyblockItemsMod.java
@@ -1,24 +1,11 @@
 package com.sujal.skyblock.items;
 
-import com.sujal.skyblock.items.factory.ItemDefinitions;
+import com.sujal.skyblock.items.registry.SkyblockRegistry;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
-import net.minecraft.server.command.CommandManager;
-import net.minecraft.text.Text;
 
 public class SkyblockItemsMod implements ModInitializer {
     @Override
     public void onInitialize() {
-        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
-            dispatcher.register(CommandManager.literal("sbitems")
-                .requires(source -> source.hasPermissionLevel(2))
-                .then(CommandManager.literal("hyperion")
-                    .executes(context -> {
-                        context.getSource().getPlayerOrThrow().getInventory().insertStack(ItemDefinitions.getHyperion());
-                        context.getSource().sendFeedback(() -> Text.of("§aGave Hyperion!"), false);
-                        return 1;
-                    }))
-            );
-        });
+        SkyblockRegistry.register(); // Loads the Creative Tab
     }
 }
