24d81a6088a3b28d7f6878d5f57731fdd5e6c85e
Sujalofficial07 <sujalbaloda32@gmail.com>
2026-01-24 17:46:55 +0530

    Update InGameHudMixin.java

--- skyblock-mixins/src/main/java/com/sujal/skyblock/mixins/client/InGameHudMixin.java
+++ skyblock-mixins/src/main/java/com/sujal/skyblock/mixins/client/InGameHudMixin.java
@@ -1,6 +1,8 @@
 package com.sujal.skyblock.mixins.client;
 
 import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -9,23 +11,27 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 @Mixin(InGameHud.class)
 public class InGameHudMixin {
 
-    // Intercept when the HUD checks for Max Health
-    @Redirect(method = "renderStatusBars", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;getMaxHealth()F"))
-    private float fakeMaxHealth(PlayerEntity player) {
-        // Visually, the player always has 20 health (10 hearts) max
-        return 20.0f;
-    }
-
-    // Intercept when the HUD checks for Current Health
+    // 1. Redirect Current Health
     @Redirect(method = "renderStatusBars", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;getHealth()F"))
     private float fakeHealth(PlayerEntity player) {
         float realMax = player.getMaxHealth();
         float realCurrent = player.getHealth();
 
-        // If health is huge (e.g., 500/1000), scale it to fit 20 (e.g., 10/20)
+        // If health is huge, scale it to 20 visually
         if (realMax > 20) {
             return (realCurrent / realMax) * 20.0f;
         }
         return realCurrent;
+    }
+
+    // 2. Redirect Max Health (Uses getAttributeValue in 1.20.1, not getMaxHealth)
+    @Redirect(method = "renderStatusBars", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;getAttributeValue(Lnet/minecraft/entity/attribute/EntityAttribute;)D"))
+    private double fakeMaxHealth(PlayerEntity player, EntityAttribute attribute) {
+        // Only fake the Max Health attribute
+        if (attribute == EntityAttributes.GENERIC_MAX_HEALTH) {
+            return 20.0;
+        }
+        // Return normal value for other attributes
+        return player.getAttributeValue(attribute);
     }
 }
