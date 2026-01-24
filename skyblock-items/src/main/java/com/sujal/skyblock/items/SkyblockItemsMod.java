bef2bc1e8a6da16e3d2754adcfebc2cc284f6b2d
Sujalofficial07 <sujalbaloda32@gmail.com>
2026-01-24 19:03:45 +0530

    Update SkyblockItemsMod.java

--- skyblock-items/src/main/java/com/sujal/skyblock/items/SkyblockItemsMod.java
+++ skyblock-items/src/main/java/com/sujal/skyblock/items/SkyblockItemsMod.java
@@ -1,5 +1,6 @@
 package com.sujal.skyblock.items;
 
+import com.sujal.skyblock.items.event.SkyblockAbilityHandler;
 import com.sujal.skyblock.items.menu.SkyblockMenuHandler;
 import com.sujal.skyblock.items.registry.SkyblockRegistry;
 import net.fabricmc.api.ModInitializer;
@@ -7,7 +8,8 @@ import net.fabricmc.api.ModInitializer;
 public class SkyblockItemsMod implements ModInitializer {
     @Override
     public void onInitialize() {
-        SkyblockRegistry.registerAll(); // Load Items & Groups
-        SkyblockMenuHandler.register(); // Load Command
+        SkyblockRegistry.registerAll(); // Load Items
+        SkyblockMenuHandler.register(); // Load Menu Command
+        SkyblockAbilityHandler.register(); // LOAD ABILITY EVENT LISTENER (Important!)
     }
 }
