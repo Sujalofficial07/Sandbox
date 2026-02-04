2d02811c4a641d96147b1ad18de1c5e05f767caf
Sujalofficial07 <sujalbaloda32@gmail.com>
2026-02-04 14:06:21 +0530

    Create VanillaOverrideHandler.java

---
+++ skyblock-core/src/main/java/com/sujal/skyblock/core/internal/protection/VanillaOverrideHandler.java
@@ -0,0 +1,24 @@
+package com.sujal.skyblock.core.internal.protection;
+
+public class VanillaOverrideHandler {
+    
+    public static boolean shouldCancelVanillaXP() {
+        return true;
+    }
+    
+    public static boolean shouldCancelVanillaHealth() {
+        return true;
+    }
+    
+    public static boolean shouldCancelVanillaHunger() {
+        return true;
+    }
+    
+    public static boolean shouldOverrideDamage() {
+        return true;
+    }
+    
+    public static boolean shouldOverrideDrops() {
+        return true;
+    }
+}
