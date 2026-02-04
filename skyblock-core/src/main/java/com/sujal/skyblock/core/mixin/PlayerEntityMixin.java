45ce019cf1fda13a80c39d9d1fe14c170097b2c7
Sujalofficial07 <sujalbaloda32@gmail.com>
2026-02-04 16:09:39 +0530

    Update PlayerEntityMixin.java

--- skyblock-core/src/main/java/com/sujal/skyblock/core/mixin/PlayerEntityMixin.java
+++ skyblock-core/src/main/java/com/sujal/skyblock/core/mixin/PlayerEntityMixin.java
@@ -1,12 +1,9 @@
 package com.sujal.skyblock.core.mixin;
 
-import com.sujal.skyblock.core.SkyBlockCore;
 import com.sujal.skyblock.core.api.stats.SkyBlockStats;
 import com.sujal.skyblock.core.api.stats.SkyBlockStatsAPI;
-import net.minecraft.entity.attribute.EntityAttribute;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.server.network.ServerPlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -18,8 +15,8 @@ import java.util.Optional;
 @Mixin(PlayerEntity.class)
 public abstract class PlayerEntityMixin {
     
-    @Inject(method = "getAttributeValue", at = @At("RETURN"), cancellable = true)
-    private void onGetAttributeValue(RegistryEntry<EntityAttribute> attribute, CallbackInfoReturnable<Double> cir) {
+    @Inject(method = "getMovementSpeed", at = @At("RETURN"), cancellable = true)
+    private void onGetMovementSpeed(CallbackInfoReturnable<Float> cir) {
         PlayerEntity player = (PlayerEntity) (Object) this;
         
         if (!(player instanceof ServerPlayerEntity serverPlayer)) {
@@ -32,15 +29,8 @@ public abstract class PlayerEntityMixin {
         }
         
         SkyBlockStats stats = statsOpt.get();
-        
-        if (attribute.matches(EntityAttributes.GENERIC_MAX_HEALTH)) {
-            cir.setReturnValue(stats.getMaxHealth());
-        } else if (attribute.matches(EntityAttributes.GENERIC_MOVEMENT_SPEED)) {
-            double speedStat = stats.getSpeed();
-            double vanillaSpeed = 0.1 * (speedStat / 100.0);
-            cir.setReturnValue(vanillaSpeed);
-        } else if (attribute.matches(EntityAttributes.GENERIC_ATTACK_DAMAGE)) {
-            cir.setReturnValue(stats.getStrength());
-        }
+        double speedStat = stats.getSpeed();
+        float vanillaSpeed = (float) (0.1 * (speedStat / 100.0));
+        cir.setReturnValue(vanillaSpeed);
     }
 }
